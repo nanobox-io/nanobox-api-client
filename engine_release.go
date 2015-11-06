89f7533b39e4ae942ad209d9847c28691f0cac4a
Steve Domino <domino.steve@gmail.com>
2015-11-06 10:40:28 -0700

    removing unnecessary fields from engine_release

--- engine_release.go
+++ engine_release.go
@@ -20,9 +20,7 @@ type (
 		Authors   []string  `json:"authors"`
 		Checksum  string    `json:"checksum"`
 		CreatedAt time.Time `json:"created_at"`
-		Generic   bool      `json:"generic"`
 		ID        string    `json:"id"`
-		Language  string    `json:"language"`
 		License   string    `json:"license"`
 		Name      string    `json:"name"`
 		Readme    string    `json:"readme"`
